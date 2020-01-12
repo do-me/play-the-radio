074946bf2061b32b10155ca4fac7ae561500a0f0
do-me <47481567+do-me@users.noreply.github.com>
2020-01-12 18:30:13 +0100

    Update play-the-radio.py

--- play-the-radio.py
+++ play-the-radio.py
@@ -1,6 +1,5 @@
 import speech_recognition as sr
 from selenium import webdriver
-browser = webdriver.Chrome(executable_path="C:/.../chromedriver.exe") 
 
 r = sr.Recognizer()
 mic = sr.Microphone()
@@ -11,5 +10,6 @@ phrase = r.recognize_google(audio)
 
 if phrase == "play the radio":
     print(phrase)
+    browser = webdriver.Chrome(executable_path="C:/.../chromedriver.exe") 
     browser.get("https://srv.deutschlandradio.de/themes/dradio/script/aod/index.html?audioMode=2&audioID=4&state=")
     browser.find_element_by_xpath('//*[@class="mkdraod-audio-play"]').click()
